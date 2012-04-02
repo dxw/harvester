5a3335d02e28b901d79df1a2c03f1b8b3a47fa94
Tom Adams <tom@holizz.com>
2012-04-02 09:15:47 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HarvesterNg::Application.config.secret_token = 'ab61857a5150c3cd5994b18f525077e581452589f8f26579e9de66922ad2333b32855c8c3d26843ad5aaaacc2dde560b227a2432ce2c8aa3574be4cea10efd2d'
