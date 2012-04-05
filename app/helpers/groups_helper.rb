5360bcab1019eeee6255e4b6823f4230d90f1b44
Tom Adams <tom@holizz.com>
2012-04-05 16:38:37 -0400

    taxonomy_tag partial

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,11 @@
 module GroupsHelper
+  def taxonomy_tag tax
+    taxonomies = {
+      attributes: {cls: ::Attribute, type: :checkboxes},
+      audiences: {cls: ::Audience, type: :text},
+      needs: {cls: ::Need, type: :text}
+    }
+
+    taxonomies[tax]
+  end
 end
