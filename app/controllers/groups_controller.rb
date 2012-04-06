df264f1fdb0d063f7bd01febbe038f6010ba5083
Tom Adams <tom@holizz.com>
2012-04-06 11:39:25 -0400

    Saving untakes and sets values

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,6 +7,26 @@ class GroupsController < ApplicationController
     if params[:commit] == 'Take'
       @group.taken_by = current_user
       @group.save!
+    elsif params[:commit] == 'Save'
+      @group.taken_by = nil
+
+      params[:pages].each do |id,page|
+        pp = Page.find(id)
+        [:attributes, :audiences, :needs].each do |tax|
+          if page[tax]
+            if page[tax].is_a? String
+              tags = page[tax].split(',').map{|s|s.strip}
+            elsif page[tax].is_a? Hash
+              tags = page[tax].keys
+            else
+              raise NotImplementedError
+            end
+
+            pp.set_tags(tax, tags)
+          end
+        end
+      end
+      @group.save!
     end
 
     redirect_to edit_group_path(@group.id)
--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -4,6 +4,8 @@ describe GroupsController do
   login_user
 
   before(:each) do
+    @users = []
+    @users << FactoryGirl.create(:user)
     @groups = []
     @groups << FactoryGirl.create(:group, name: 'Group1')
     @groups << FactoryGirl.create(:group, name: 'Group2')
@@ -11,11 +13,31 @@ describe GroupsController do
 
   describe "POST 'update'" do
     it "allows taking groups" do
-      pending
-      post 'update', group: {id: @groups.first.id, taken: true}
+      p 'a'
+      post 'update', {id: @groups.first.id, commit: 'Take'}
+      p 'b'
 
       @groups.first.taken?.should be_true
-      @groups.taken_by_user.should_not be_nil
+      @groups.first.taken_by_user.should_not be_nil
+      response.should be_redirect
+    end
+
+    it "allows untaking groups" do
+      @groups.first.taken_by = @users.first.id
+
+      post 'update', {id: @groups.first.id, commit: 'Save'}
+
+      @groups.first.taken?.should be_false
+      @groups.first.taken_by_user.should == @users.first.id
+      response.should be_redirect
+    end
+
+    it "allows updating values" do
+      pending
+      post 'update', group: {id: @groups.first.id, commit: true}
+
+      @groups.first.taken?.should be_true
+      @groups.first.taken_by_user.should_not be_nil
       response.should be_redirect
     end
   end
