59f0ac25776f1da1e7f48109a3cb492375622a4e
Harry Metcalfe <harry@thedextrousweb.com>
2012-04-10 12:29:04 +0100

    Added my account to the department in test data

--- manual-testing/seeds_test.rb
+++ manual-testing/seeds_test.rb
@@ -1,12 +1,13 @@
-user = User.create!(email: 'tom@dxw.com', password: 'foobar')
-user = User.create!(email: 'harry@dxw.com', password: 'foobar')
+user1 = User.create!(email: 'tom@dxw.com', password: 'foobar')
+user2 = User.create!(email: 'harry@dxw.com', password: 'foobar')
 
 department = Department.create!(name: 'Ministry of Peas')
 
 group1 = Group.create!(name: 'Broccoli')
 group2 = Group.create!(name: 'Cabbage')
 
-department.users << user
+department.users << user1
+department.users << user2
 department.groups << group1
 department.groups << group2
 
