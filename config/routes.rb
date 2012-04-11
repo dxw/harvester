6b0cbad039991abbb6545969d5c3b5a5bae1d93a
Tom Adams <tom@holizz.com>
2012-04-11 16:34:10 -0400

    Make routes.rb a little bit nicer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 HarvesterNg::Application.routes.draw do
-  post 'groups/get_a_group'
-  resources :groups do
+  resources :groups, only: [:edit, :update, :index] do
+    post :get_a_group, on: :collection
     get :autocomplete_audience_name, on: :collection
     get :autocomplete_need_name, on: :collection
   end
