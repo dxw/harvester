8ded44c0368efa0ef9dcc0150a2ebf2a491cea47
Harry Metcalfe <harry@thedextrousweb.com>
2012-04-18 12:12:19 +0100

    Put the list before the add field

--- app/assets/javascripts/needs.js
+++ app/assets/javascripts/needs.js
@@ -2,7 +2,8 @@
 $(document).ready(function() {
   $("input.editable_list").each(function(i, input) {
     $('#' + input.id).tagEditor({
-      confirmRemoval: true
+      confirmRemoval: true,
+      tagsBeforeField: true
     });
   });
 });
