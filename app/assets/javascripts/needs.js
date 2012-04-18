0061cfa01ba8b363022a67d843a07855b24c9b21
Harry Metcalfe <harry@thedextrousweb.com>
2012-04-18 13:27:44 +0100

    Don't confirm removal; move tags to before the input

--- app/assets/javascripts/needs.js
+++ app/assets/javascripts/needs.js
@@ -2,7 +2,6 @@
 $(document).ready(function() {
   $("input.editable_list").each(function(i, input) {
     $('#' + input.id).tagEditor({
-      confirmRemoval: true,
       tagsBeforeField: true
     });
   });
