6b93d2167e58fd1ad9409c0ff864ee9e1cf8255b
Tom Adams <tom@holizz.com>
2012-06-15 10:56:23 -0400

    CRUD users

---
+++ app/admin/users.rb
@@ -0,0 +1,46 @@
+ActiveAdmin.register User do
+
+  form do |f|
+    f.inputs "Details" do
+      f.input :email
+      if f.object.new_record?
+        f.input :password
+      end
+    end
+    f.buttons
+  end
+
+  # Everything below this line is to hide encrypted_password
+
+  sections = %w[
+    id
+    email
+    sign_in_count
+    current_sign_in_at
+    last_sign_in_at
+    current_sign_in_ip
+    last_sign_in_ip
+    created_at
+    updated_at
+  ].map{|s|s.to_sym}
+
+  index do
+    sections.each do |s|
+      column s
+    end
+    default_actions
+  end
+
+  show do
+    attributes_table do
+      sections.each do |s|
+        row s
+      end
+    end
+    active_admin_comments
+  end
+
+  sections.each do |s|
+    filter s unless s == :id
+  end
+end
