86d7ac33ab6a1eb97a6ef8dbc5deeebf0133b0ae
Harry Metcalfe <harry@thedextrousweb.com>
2012-06-19 10:47:02 +0100

    Correct email domain for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,6 @@ HarvesterNg::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.default_url_options = {
-    :host => 'harvester.staging.dxw.uk0.bigv.io'
+    :host => 'harvester.prod.dxw.net'
   }
 end
